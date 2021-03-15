6544b3baac41b852cf2faf36a282b772c6927332
Thibault <thibaultfeugere@gmail.com>
2021-03-15 19:33:48 +0100

    Addign beginning of OTP system

--- app/routes/otp.js
+++ app/routes/otp.js
@@ -1,14 +1,40 @@
+const { text } = require('body-parser');
 const express = require('express');
 const router = express.Router();
+const otpGenerator = require('otp-generator')
 const nodemailer = require('nodemailer');
+const config = require('../config/config.json');
+
+const transporter = nodemailer.createTransport(
+  config.gmailConfig
+);
+
+const otp = otpGenerator.generate(6, { upperCase: true, specialChars: false });
 
 // Temporary because i am waiting the Redis DB
 const emailDb = "thibault.feugere@ynov.com";
 
-router.get('/', function (req, res, next) {
+router.post('/', function (req, res, next) {
   res.setHeader('Content-Type', 'application/json');
   if (req.body.email == emailDb) {
-
+    const mailOptions = {
+      from: 'intersport.team.france@gmail.com',
+      to: req.body.email,
+      subject: 'Your OTP !',
+      text: 'Your OTP is : ' + otp,
+      html: 'Your OTP is : <strong>' + otp + '</strong>'
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response + ' with OTP : ' + otp);
+        res.end('Email sent !');
+      }
+    });
+  } else {
+    res.end('Error');
   }
 });
 
