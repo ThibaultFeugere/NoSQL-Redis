28834bdf6178fe1d43b722e0b9451f0fd3c6b378
Thibault <thibaultfeugere@gmail.com>
2021-03-15 17:26:46 +0100

    Cleaning the directory

--- app/routes/otp.js
+++ app/routes/otp.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const nodemailer = require('nodemailer');
+
+// Temporary because i am waiting the Redis DB
+const emailDb = "thibault.feugere@ynov.com";
 
 router.get('/', function (req, res, next) {
   res.setHeader('Content-Type', 'application/json');
-  if (req.body.email) {
-    
+  if (req.body.email == emailDb) {
+
   }
 });
 
