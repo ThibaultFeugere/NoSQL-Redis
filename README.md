42e34e7abc73c856be5ef540c865e7a7e4d95be7
Thibault <thibaultfeugere@gmail.com>
2021-03-15 14:59:12 +0100

    Creation of the squeleton

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # No SQL - TP Redis
 
+## Equipe
+
+- MAURY Louis
+- DEMANECHE Antonin
+- FEUGERE Thibault
+
+## Installation de Redis
+
+`npm install redis`
+
+## Installation de IoRedis
+
+`npm install ioredis`
+
+## Installation de http-errors
+
+`npm install http-errors`
+
+## Installation de Express JS
+
+`npm install express`
---
+++ app.js
@@ -0,0 +1,40 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+
+// Importations des routes
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+const PORT = 1337;
+
+app.listen(PORT, () => {
+  console.log("Express Node.js server running on port " + PORT)
+})
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
