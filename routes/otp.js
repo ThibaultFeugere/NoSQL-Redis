a3bcd7a474a62d687b25a09d42143f75a9aaf3a1
Thibault <thibaultfeugere@gmail.com>
2021-03-15 16:48:31 +0100

    Removing useless comment

--- routes/otp.js
+++ routes/otp.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
-  console.log('otp')
+  res.setHeader('Content-Type', 'application/json');
+  if (req.body.email) {
+    
+  }
 });
 
 module.exports = router;
