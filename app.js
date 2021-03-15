74807724132325eadf814adf146c062fd976a875
Thibault <thibaultfeugere@gmail.com>
2021-03-15 15:29:02 +0100

    Adding routes

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const path = require('path');
 
 // Importations des routes
 const indexRouter = require('./routes/index');
+const loginRouter = require('./routes/login');
+const logoutRouter = require('./routes/logout');
+const otpRouter = require('./routes/otp');
+const registerRouter = require('./routes/register');
 
 const app = express();
 
@@ -20,6 +24,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', indexRouter);
+app.use('/login', loginRouter);
+app.use('/logout', logoutRouter);
+app.use('/otp', otpRouter);
+app.use('/register', registerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
