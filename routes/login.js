d52e344f51faaececae845c1a034a97967019f05
Thibault <thibaultfeugere@gmail.com>
2021-03-15 16:48:58 +0100

    Adding beginning of login system with email and hash check

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  console.log('login')
+// Temporary because i am waiting the Redis DB
+const emailDb = "thibault.feugere@ynov.com";
+const hashDb = "$2b$12$id5DcgsbZfK2B65VKvWhdezakOzuVpuUeMj7RpKq11ZOWcSOY9qhu"; // hash = "mdp"
+
+router.post('/', function (req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    if (req.body.email && req.body.password) {
+        // Store vars
+        const email = req.body.email;
+        const password = req.body.password;
+
+        bcrypt.compare(password, hashDb, function(err, result) {
+          if (email == emailDb && result == true) {
+            res.send('Email et mdp valide');
+          } else {
+            res.send('Email ou mdp invalide');
+          }
+        });
+  }
 });
 
 module.exports = router;
