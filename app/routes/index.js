86f3f708a8bbd2a2942c26cfb051d0eed88c2e37
Thibault <thibaultfeugere@gmail.com>
2021-03-20 10:41:47 +0100

    Removing useless parameters

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function (req, res, next) {
+router.get('/', function () {
   console.log('Projet réalisé par Louis, Antonin, Steven et Thibault')
 });
 
