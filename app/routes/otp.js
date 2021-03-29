c1ec2662b0dd51b1c1d495a39089594f1a499d2b
louis_maury <louis3340@gmail.com>
2021-03-29 14:50:33 +0200

    Fix otp.js

--- app/routes/otp.js
+++ app/routes/otp.js
@@ -12,15 +12,11 @@ const transporter = nodemailer.createTransport(
   config.gmailConfig
 );
 
-const otp = otpGenerator.generate(6, { upperCase: true, specialChars: false }).toUpperCase();
 
-Date.prototype.addMinutes = function(minutes) {
-    this.setMinutes(this.getMinutes() + minutes);
-    return this;
-};
 
 router.post('/', function (req, res, next) {
     res.setHeader('Content-Type', 'application/json');
+    const otp = otpGenerator.generate(6, { upperCase: true, specialChars: false }).toUpperCase();
     // On vérifie qu'il y a bien un email de fournit dans la requête POST
     if (req.body.email !== undefined && req.body.email !== null) {
         // On vérifie que l'email est au format correcte
@@ -32,7 +28,7 @@ router.post('/', function (req, res, next) {
                     // On vérifie qu'il n'est pas connecté pour envoyer l'email
                     if (obj.login == 0) {
                         // On vérifie qu'il n'y a pas de code OTP déjà envoyé dans les 5 dernières minutes
-                        if (obj.otpValidity < Date.now()) {
+                        if (obj.otpValidity < (Date.now())) {
                             const mailOptions = {
                                 from: 'intersport.team.france@gmail.com',
                                 to: obj.email,
@@ -40,13 +36,13 @@ router.post('/', function (req, res, next) {
                                 text: 'Your OTP is : ' + otp,
                                 html: 'Your OTP is : <strong>' + otp + '</strong>'
                             };
-                            client.hset("email:" + obj.email, "otp", otp, "otpValidity", new Date.addMinutes(5), function (err, obj) {
+                            client.hset("email:" + obj.email, "otp", otp, "otpValidity", (new Date().setSeconds(new Date().getSeconds() + 300)), function (err, obj) {
                                 transporter.sendMail(mailOptions, function(error, info){
                                     if (error) {
                                         console.log(error);
                                     } else {
                                         console.log("Email sent: " + info.response + " with OTP : " + otp);
-                                        res.end("Email sent to " + obj.email + " valid for 5 minutes.");
+                                        res.end("Email sent.");
                                     }
                                 });                    
                             });
@@ -54,14 +50,14 @@ router.post('/', function (req, res, next) {
                             res.send("Un code vous a déjà été envoyé au cours de ces 5 dernières minutes.");
                         }
                     } else {
-                        res.send("Compte déjà connecté.")
+                        res.send("Erreur");
                     }
                 } else {
                     res.send("Erreur");
                 }
             });
         } else {
-            res.send("Email invalide.")
+            res.send("Email");
         }
     } else {
         res.send("Un email est nécessaire.");
